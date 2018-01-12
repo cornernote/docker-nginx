2dd9a3927039f525cb995b06f7914d0bfa28418f
Brett O'Donnell <cornernote@gmail.com>
2018-01-12 14:20:25 +1030

    remove sed changes to config

--- files/run.sh
+++ files/run.sh
@@ -13,22 +13,6 @@ log() {
 # Fail on any error
 set -o errexit
 
-# Example of using environment variable in configuration at runtime
-if [ ! -n "$NGINX_ERROR_LOG_LEVEL" ] ; then
-    NGINX_ERROR_LOG_LEVEL="warn"
-fi
-sed -i "s|\${NGINX_ERROR_LOG_LEVEL}|${NGINX_ERROR_LOG_LEVEL}|" /etc/nginx/nginx.conf
-
-if [ ! -n "$SERVER_NAME" ] ; then
-    SERVER_NAME="app"
-fi
-sed -i "s|\${SERVER_NAME}|${SERVER_NAME}|" /etc/nginx/nginx.conf
-
-if [ ! -n "$FASTCGI_PASS_HOST" ] ; then
-    FASTCGI_PASS_HOST="php"
-fi
-sed -i "s|\${FASTCGI_PASS_HOST}|${FASTCGI_PASS_HOST}|" /etc/nginx/nginx.conf
-
 # Generate certificates
 if [ ! -n "$DH_SIZE" ] ; then
     DH_SIZE=2048
