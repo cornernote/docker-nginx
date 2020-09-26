3cf8662c2eb91c797eb82e27e9b5845d956e4da2
cornernote <cornernote@gmail.com>
2020-09-27 01:56:31 +0930

    better non-ssl handling

--- files/run.sh
+++ files/run.sh
@@ -13,28 +13,27 @@ log() {
 # Fail on any error
 set -o errexit
 
-# Generate certificates
-if [ ! -n "$DH_SIZE" ] ; then
-    DH_SIZE=2048
-fi
-if [ ! -f /etc/dhparam/dhparam.pem ]; then
-    log "dhparam file /etc/dhparam/dhparam.pem does not exist"
-    openssl dhparam -out /etc/dhparam/dhparam.pem $DH_SIZE || die "Could not generate dhparam file"
-fi
-
 # Generate nginx config from template
 envsubst '$ERROR_LOG_LEVEL $SERVER_NAME $FASTCGI_PASS_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Toggle SSL
-if [ "$USE_SSL" = true ] ; then
-    log "SSL is enabled, uncommenting nginx.conf"
-    sed -i -r 's/#?;#//g' /etc/nginx/nginx.conf
+if [ "$USE_SSL" = 1 ] ; then
+    # Generate certificates
+    if [ ! -n "$DH_SIZE" ] ; then
+        DH_SIZE=2048
+    fi
+    if [ ! -f /etc/dhparam/dhparam.pem ]; then
+        log "dhparam file /etc/dhparam/dhparam.pem does not exist"
+        openssl dhparam -out /etc/dhparam/dhparam.pem $DH_SIZE || die "Could not generate dhparam file"
+    fi
+    #sed -i -r 's/#?;#//g' /etc/nginx/nginx.conf
 else
     log "SSL is disabled, commenting nginx.conf"
-    sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/nginx.conf
+    sed -i -r 's/(return .*301)/#;#\1/g; s/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/nginx.conf
 fi
 
 # Run nginx
+log "Starting nginx"
 nginx # -g 'daemon off;'
 
 # Check if config or certificates were changed
